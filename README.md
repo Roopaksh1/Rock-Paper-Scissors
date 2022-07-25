55d21f234581564db6824328c8f5b483be00b415
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2022-07-25 16:31:39 +0530

    Add UI

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rock-Paper-Scissors
+
+Images Featured
+
+background-img :- https://www.istockphoto.com/photo/rock-paper-scissors-gm149424857-20529215?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=http%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Frock%2520paper%2520scissor%2F&utm_term=rock+paper+scissor
--- script.js
+++ script.js
@@ -2,54 +2,116 @@ let playerSelection;
 let computerSelection;
 let computerScore = 0;
 let playerScore = 0;
+let round = 0;
+
+const heading = document.querySelector(".heading");
+const menu = document.querySelector(".menu");
+const startButton = document.querySelector(".start-btn");
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorButton = document.querySelector(".scissor");
+const homeButton = document.querySelector(".home-btn");
+const gameScreen = document.querySelector(".game-screen");
+
+startButton.addEventListener("click", Start);
+rockButton.addEventListener("click", game);
+paperButton.addEventListener("click", game);
+scissorButton.addEventListener("click", game);
+homeButton.addEventListener("click", home);
+
+function home() {
+    gameScreen.classList.add("hidden");
+    menu.classList.remove("hidden");
+    document.body.style.background = "url(images/bg.jpg) no-repeat center center fixed";
+    document.body.style.backgroundSize = "100rem 100rem";
+}
+
+function Start() {
+    reset();
+    heading.textContent = "Choose Your Object";
+    menu.classList.add("hidden");
+    gameScreen.classList.remove("hidden");
+    document.body.style.background = "black";
+}
+
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0)
-        return "rock";
+        return "Rock";
     else if (choice === 1)
-        return "paper";
+        return "Paper";
     else
-        return "scissor";
+        return "Scissor";
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissor") {
-        if ((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissor" && computerSelection === "paper")) {
-            console.log(`YOU WIN! ${playerSelection} beats ${computerSelection}`);
+    if (playerSelection === "Rock" || playerSelection === "Paper" || playerSelection === "Scissor") {
+        if ((playerSelection === "Rock" && computerSelection === "Scissor") ||
+            (playerSelection === "Paper" && computerSelection === "Rock") ||
+            (playerSelection === "Scissor" && computerSelection === "Paper"))
             return 1;
-        }
-        else if (playerSelection === computerSelection) {
-            console.log("It's a TIE");
+        else if (playerSelection === computerSelection)
             return 0;
-        }
-        else {
+        else
             console.log(`YOU LOSE! ${computerSelection} beats ${playerSelection}`);
-            return -1;
-        }
+        return -1;
     }
-    else
-        alert("Enter valid choice!");
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
+function game(e) {
 
-        computerSelection = getComputerChoice();
-        playerSelection = prompt("Enter your choice").toLowerCase();
+    computerSelection = getComputerChoice();
+    playerSelection = e.target.textContent;
 
-        let result = playRound(playerSelection, computerSelection);
-        if (result === 1)
-            playerScore++;
-        else if (result === -1)
-            computerScore++;
-        console.log(`${playerScore} - ${computerScore}`)
+    const pScore = document.querySelector(".player-score");
+    const cScore = document.querySelector(".computer-score");
+    let result = playRound(playerSelection, computerSelection);
+    round++;
+
+    if (result === 1)
+        playerScore++;
+    else if (result === -1)
+        computerScore++;
+
+    pScore.textContent = `${playerScore}`;
+    cScore.textContent = `${computerScore}`;
+
+    const resultText = document.querySelector(".result-screen");
+    const resultDeclare = document.querySelector(".result-declare");
+
+    resultText.children[round].children[0].textContent = `${playerSelection}`;
+    resultText.children[round].children[1].textContent = `${computerSelection}`;
+
+    if (round === 5) {
+        rockButton.classList.add("hidden");
+        paperButton.classList.add("hidden");
+        scissorButton.classList.add("hidden");
+        if (playerScore > computerScore)
+            resultDeclare.textContent = `Player Wins! ${playerScore} - ${computerScore}`
+        else if (playerScore === computerScore)
+            resultDeclare.textContent = `DRAW! ${playerScore} - ${computerScore}`
+        else
+            resultDeclare.textContent = `Computer Wins! ${playerScore} - ${computerScore}`
     }
+}
 
-    if (playerScore > computerScore)
-        console.log(`Player Wins! ${playerScore} - ${computerScore}`)
-    else if (playerScore === computerScore)
-        console.log(`DRAW! ${playerScore} - ${computerScore}`)
-    else
-        console.log(`Computer Wins! ${playerScore} - ${computerScore}`)
+function reset() {
+    computerScore = 0;
+    playerScore = 0;
+    round = 0;
+    rockButton.classList.remove("hidden");
+    paperButton.classList.remove("hidden");
+    scissorButton.classList.remove("hidden");
+
+    const pScore = document.querySelector(".player-score");
+    const cScore = document.querySelector(".computer-score");
+    pScore.textContent = 0;
+    cScore.textContent = 0;
+
+    const resultText = document.querySelector(".result-screen").children;
+    for(let i = 1; i < resultText.length; i++){
+        resultText[i].children[0].textContent = "";
+        resultText[i].children[1].textContent = "";
+    }
 }
